8dee3ba90c3ab739053d67212d84642e5124ce48
agent47daun <agent47daun@gmail.com>
2022-05-17 11:49:32 +0400

    Эндпоинт товара с кнопкой покупки

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import BuyItemView
+from .views import BuyItemView, ItemDetailView
 
 
 urlpatterns = [
-    path("buy/<int:pk>/", BuyItemView.as_view())
+    path("buy/<int:pk>/", BuyItemView.as_view()),
+    path("item/<int:pk>/", ItemDetailView.as_view())
 ]
--- main/views.py
+++ main/views.py
@@ -1,3 +1,6 @@
+from django.conf import settings
+from django.shortcuts import render
+from django.views import View
 from rest_framework.generics import GenericAPIView
 from rest_framework.exceptions import NotFound
 from rest_framework.response import Response
@@ -19,3 +22,15 @@ class BuyItemView(GenericAPIView):
         session_id = get_stripe_session_id(item, cancel_url, success_url)
 
         return Response({"session_id": session_id}, 201)
+
+
+class ItemDetailView(View):
+
+    def get(self, request, pk):
+        try:
+            item = Item.objects.get(id=pk)
+        except Item.DoesNotExist:
+            raise NotFound()
+
+        stripe_key = settings.STRIPE_PUBLIC_KEY
+        return render(request, "checkout.html", context={"item": item, "stripe_key": stripe_key})
