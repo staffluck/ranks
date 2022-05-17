615868e8f6cd0ac006b0550ca2cbdfca18505df4
agent47daun <agent47daun@gmail.com>
2022-05-17 11:31:19 +0400

    Эндпоинт для создания session_id

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 
+from .views import BuyItemView
+
 
 urlpatterns = [
-    path("buy/", )
+    path("buy/<int:pk>/", BuyItemView.as_view())
 ]
--- main/views.py
+++ main/views.py
@@ -1,7 +1,21 @@
 from rest_framework.generics import GenericAPIView
+from rest_framework.exceptions import NotFound
+from rest_framework.response import Response
+
+from .models import Item
+from .stripe_utils import get_stripe_session_id
 
 
 class BuyItemView(GenericAPIView):
 
     def get(self, request, pk):
-        pass
+        try:
+            item = Item.objects.filter(id=pk)
+        except Item.DoesNotExist:
+            raise NotFound
+
+        cancel_url = request.build_absolute_uri("")
+        success_url = request.build_absolute_uri("")
+        session_id = get_stripe_session_id(item, cancel_url, success_url)
+
+        return Response({"session_id": session_id}, 201)
